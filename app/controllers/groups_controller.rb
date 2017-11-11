1a7b3c4d42f59b0009960bfd063917e6718893eb
lilingiu <lilingxiu@gmail.com>
2017-11-11 16:11:36 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,25 @@ class GroupsController < ApplicationController
     @group = Group.all
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
-  end 
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+
+      redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
 end
